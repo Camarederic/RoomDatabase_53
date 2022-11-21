45d8b08ee526ca77b88769aed4b986e8cc844c15
Camarederic <camarederic@mail.ru>
2022-11-21 20:27:07 +0300

    Create Fragments and Navigation Graph

---
+++ app/src/main/java/com/example/roomdatabase_53/fragments/list/ListFragment.kt
@@ -0,0 +1,23 @@
+package com.example.roomdatabase_53.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.roomdatabase_53.R
+
+
+class ListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+}
