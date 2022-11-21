eed54fa2cb8e11abd9b51ce4491c5cec57ebcabc
Camarederic <camarederic@mail.ru>
2022-11-21 23:08:41 +0300

    Create RecyclerView Adapter

---
+++ app/src/main/java/com/example/roomdatabase_53/fragments/list/ListAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.roomdatabase_53.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdatabase_53.R
+import com.example.roomdatabase_53.data.User
+import kotlinx.android.synthetic.main.custom_row.view.*
+
+class ListAdapter:RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    private var userList = emptyList<User>()
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.custom_row, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = userList[position]
+        holder.itemView.textViewId.text = currentItem.id.toString()
+        holder.itemView.textViewFirstName.text = currentItem.firstName
+        holder.itemView.textViewLastName.text = currentItem.lastName
+        holder.itemView.textViewAge.text = currentItem.age.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    fun setData(user: List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+    }
+}
